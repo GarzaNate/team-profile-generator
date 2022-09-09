3b9a8ab9c5e8c5efe69b53d4dd7012283635b2ac
slendyG <josuegarza0608@gmail.com>
2022-09-08 23:08:56 -0700

    Add classes for each type of employee

---
+++ index.js
@@ -0,0 +1,8 @@
+// node modules
+const inquirer = require("inquirer");
+const fs = require("fs")
+
+// lib modules
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
