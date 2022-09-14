75da46c49d450539d40d6415fcda768ecf7ec589
slendyG <josuegarza0608@gmail.com>
2022-09-13 22:30:58 -0700

    Add function to generate html template and added my README

---
+++ README.md
@@ -0,0 +1,31 @@
+
+# Team-Profile Generator
+
+## Description
+This project is meant to build a dynamic html webpage that displays a full team of who you are working with. This is done by running the application in the CLI and answering a few simple questions.
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+## Installation
+Must have npm installed as well as inquirer.
+
+## Usage
+In the CLI or command line. It is started with node index.js.
+
+## Tests
+Must have jest installed to run tests.
+
+## Credits
+N/A
+
+
+## License
+![Apache_2.0](https://img.shields.io/badge/LICENSE-Apache_2.0-blueviolet)
+
+## Contact
+* Email: nategarza2410@gmail.com
+* Github: GarzaNate
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
 
 // array for members answers
-const teamMemberData = []
+const teamMember = []
 
 // asysnc function for questions based on role
 const questions = async () => {
@@ -44,66 +44,66 @@ const questions = async () => {
             },
         ])
 
-        // questions if manager is selected
-        if(answers.role === "Manager") {
-            const officeAns = await inquirer
-                .prompt([
-                    {
-                        type: "input",
-                        message: "What is your office number?",
-                        name: "officeNumber",
-                    }
-                ])
-        
-            // adding manager class
-            const newManager = new Manager (
+    // questions if manager is selected
+    if (answers.role === "Manager") {
+        const officeAns = await inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is your office number?",
+                    name: "officeNumber",
+                }
+            ])
+
+        // adding manager class
+        const newManager = new Manager(
             answers.name,
             answers.id,
             answers.email,
-            answers.officeNumber,
-            )
-            teamMemnber.push(newManager);
+            officeAns.officeNumber,
+        )
+        teamMember.push(newManager);
 
         // else if for engineer questions
-        } else if (answers.role === "Engineer") {
-            const githubAns = await inquirer
-                .prompt([
-                    {
-                        type: "input",
-                        message: "What is your github username?",
-                        name: "github",
-                    }
-                ])
+    } else if (answers.role === "Engineer") {
+        const githubAns = await inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is your github username?",
+                    name: "github",
+                }
+            ])
 
-            // adding engineer class
-            const newEngineer = new Engineer (
-                answers.name,
-                answers.id,
-                answers.email,
-                answers.github,
-            );
-            teamMemnber.push(newEngineer);
-       
+        // adding engineer class
+        const newEngineer = new Engineer(
+            answers.name,
+            answers.id,
+            answers.email,
+            githubAns.github,
+        );
+        teamMember.push(newEngineer);
+
         // else if for intern questions
-        } else if (answers.role === "Intern") {
-            const schoolAns = await inquirer
-                .prompt([
-                    {
-                        type: "input",
-                        message: "What school did you attend?",
-                        name: "school"
-                    }
-                ])
-            
-            // adding intern class
-            const newIntern = new Intern (
-                answers.name,
-                answers.id,
-                answers.email,
-                answers.school,
-            );
-            teamMemnber.push(newIntern);
-        }
+    } else if (answers.role === "Intern") {
+        const schoolAns = await inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What school did you attend?",
+                    name: "school"
+                }
+            ])
+
+        // adding intern class
+        const newIntern = new Intern(
+            answers.name,
+            answers.id,
+            answers.email,
+            schoolAns.school,
+        );
+        teamMember.push(newIntern);
+    }
 };
 
 // function to add member / create team
@@ -118,25 +118,95 @@ async function promptQuestions() {
                 name: "newMember",
                 choices: ["Add another member?", "Create Team"],
             }
-        ]);
+        ]).then(answers => {
+            console.log(answers)
 
-        if(addMember.newMember === "Add another member") {
-            return promptQuestions();
-        }
-        return createTeam()
+            answers.newMember === "Add another member?" ? promptQuestions() : createTeam()
+        })
+
+    // if(addMember.newMember === "Add another member") {
+    //     console.log('alive')
+    //     return promptQuestions();
+    // }
+    // console.log('check')
+    // return createTeam()
 };
 
 promptQuestions();
 
 // function to create team
 function createTeam() {
-    console.log("New Team", teamMemberData);
+    console.log("New Team", teamMember);
     fs.writeFile("./dist/index.html",
-    generateHTML(teamMemberData),
-    "utf-8"
+        generateHTML(teamMember),
+        (err) =>
+            err ? console.error(err) : console.log('Building HTML page...')
     );
 };
-function generateHTML(data){
-    return `
-    `
+function generateHTML(data) {
+    console.log(data)
+    let cardTemplate = ``
+    data.forEach(element => {
+        const role = element.getRole()
+        let uniqueDataPoint = ''
+        switch (role) {
+            case "Manager":
+                uniqueDataPoint = element.officeNumber;
+                break;
+            case "Engineer":
+                uniqueDataPoint = element.github;
+                break;
+            case "Intern":
+                uniqueDataPoint = element.school;
+                break;
+        }
+        return cardTemplate += `
+        <div class="card employee-card">
+            <div class="card-header">
+                <h2 class="card-title">${element.getRole()}</h2>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">${element.name}</li>
+                    <li class="list-group-item">${element.id}</li>
+                    <li class="list-group-item">${element.email}</li>
+                    <li class="list-group-item">${uniqueDataPoint}</li>
+                </ul>
+            </div>
+        </div>
+        `
+    });
+    return `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Team</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    </head>
+    
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 jumbotron mb-3 team-heading">
+                    <h1 class="text-center">My Team</h1>
+                </div>
+            </div>
+        </div>
+    
+        <div class="container">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-center">
+                    
+                <!--Swap static card w employee info -->
+                ${cardTemplate}
+            </div>
+        </div>
+    
+    </body>
+    
+    </html>`
 }
